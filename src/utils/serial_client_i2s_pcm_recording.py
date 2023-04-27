3bdde5182e4466e1485e81a2df6284d30f10bbf6
citrovin <94009181+citrovin@users.noreply.github.com>
2023-04-27 18:27:16 +0200

    Recording of audio with board possible

--- src/utils/serial_client_i2s_pcm_recording.py
+++ src/utils/serial_client_i2s_pcm_recording.py
@@ -8,8 +8,12 @@ import sys
 import time
 import os
 
-def main(dev: str='/dev/cu.usbmodem1103', baudrate: int=921600, timeout: int=10):
-	with open('out.pcm', 'wb') as f:
+def main(dev: str='/dev/cu.usbmodem1103', baudrate: int=921600, timeout: int=10, data_path: str='./data/'):
+
+	file_path = get_path(data_path=data_path)
+	print(f'File created in: {file_path}')
+
+	with open(file_path, 'wb') as f:
 		print(f'Waiting on {dev}…')
 		ser = None
 		while ser is None:
@@ -26,14 +30,10 @@ def main(dev: str='/dev/cu.usbmodem1103', baudrate: int=921600, timeout: int=10)
 			try:
 				r = ser.readline() # Read one line of data
 				f.write(r[:-2]) # Write raw data, discarding \r\n
-				# print(r)
-				
-				# r = ser.read(96) # Read one line of data
-				# r = ser.read(64) # Read one line of data
-				# f.write(r) # Write raw data, discarding \r\n
 				
 			except KeyboardInterrupt:
 				print('\nRecording data interrupted with KeyboardInterrupt: closing and saving file.')
+				f.close()
 				ser.close()
 				f.close()
 				break
@@ -51,7 +51,47 @@ def main(dev: str='/dev/cu.usbmodem1103', baudrate: int=921600, timeout: int=10)
 							pass
 					time.sleep(0.1)
 					
-            
+
+def get_path(data_path: str='./data/'):
+	
+	choice=int(input('Choose language: \n'
+       + '1. German\n' 
+	   + '2. English\n'  
+       + '3. French\n'
+       + '4. Italian\n'
+       + '5. Test (e.g. tuning gain of mic)\n'
+       
+	   )
+	)
+
+	file_path = data_path
+	
+	if(choice==1):
+		file_path += 'german/'
+	elif(choice==2):
+		file_path += 'engilsh/'
+	elif(choice==3):
+		file_path += 'french/'
+	elif(choice==4):
+		file_path += 'italian/'
+	elif(choice==5):
+		file_path += 'test/'
+	else:
+		print('LanguageError: language does not exist in the data directory.')
+
+	
+	specs = input('Name of recording: ')
+	specs += '.pcm'
+	file_path += specs
+
+
+	return file_path
+
+
 
 if __name__ == '__main__':
 	main(*sys.argv[1:])
+
+	# file_path = get_path()
+	# print(file_path)
+	
